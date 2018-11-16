6455bf6e3dd1513486082fa9581fbf7ad17c4548
Justin Espiritu <Espiritu.L.Justin@gmail.com>
2018-11-15 18:47:03 -0600

    initialize

---
+++ A10.java
@@ -0,0 +1,92 @@
+/* A10.java - Driver code for this assignment
+*
+*  @version CS 321 - Fall 2018 - A10
+*
+*  @author 1st STUDENT'S FULL NAME GOES HERE
+*
+*  @author 2nd STUDENT'S FULL NAME GOES HERE (DELETE THIS LINE IF NOT NEEDED)
+*
+*  @author 3rd STUDENT'S FULL NAME GOES HERE (DELETE THIS LINE IF NOT NEEDED)
+*
+*/
+
+import java.util.*;
+import java.io.*;
+
+class A10 {
+
+  static int size;        // number of edges on one side of the square
+  static int minTurns;    // minimum required number of turns in the solution
+  static int turnPenalty; // penalty for each extra turn over the minimum
+
+  /* define your static variables below (NO instance variables allowed) */
+
+
+  /* load problem definition from the given file into applicable static
+   * variables.
+   * This method does not send anything to the console. It must catch applicable
+   * exceptions and output meaningful error messages.
+   */
+  static void loadProblem(String fileName) {
+
+    /* To be completed */
+
+  }// loadProblem method
+
+  /* solve the problem (previously defined using the loadProblem method) using
+   * dynamic programming. Both the optimal value and an optimal solution must
+   * be computed and stored in/retrievable from static variables, using the
+   * following two methods.
+   * This method does not send anything to the console.
+   */
+  static void solveProblem() {
+
+    /* To be completed */
+
+  }// solveProblem method
+
+  /* return the value of an optimal solution obtained with the solveProblem
+   * method.
+   * This method does not send anything to the console.
+   * You can assume that I will always call this method right before calling
+   * the getOptimalSolution method below.
+   */
+  static int getOptimalValue() {
+
+    /* To be completed */
+
+    return -1;
+  }// getOptimalValue method
+
+  /* return an optimal solution (e.g., "LRRLLLRR") obtained with the
+   * solveProblem method.
+   * This method does not send anything to the console.
+   * You can assume that I will always call getOptimalValue right before calling
+   * this method.
+   */
+  static String getOptimalSolution() {
+
+    /* To be completed */
+
+    return "";
+  }// getOptimalValue method
+
+
+  /* Implement any helper method(s) that you need (if any) below
+   * All methods in this class MUST be static.
+   */
+
+
+  /* Do not modify this method in your submission
+   */
+  public static void main(String[] args) {
+
+    String fileName = args[0];
+
+    loadProblem(fileName);
+    solveProblem();
+    int val = getOptimalValue();
+    System.out.println( getOptimalSolution() + " " + val );
+  }// main method
+
+}// A10 class
